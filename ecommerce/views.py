81116fe5b294b0b30be3ff461c75aa2a439ec1ab
Floyd Kots <floydkotohoyoh@gmail.com>
2018-01-19 18:52:16 +0300

    Setup contact form

---
+++ ecommerce/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from .forms import ContactForm
+
+
+def home_page(request):
+    context = {
+        "title": "Home Page",
+        "content": "Welcome to the homepage"
+    }
+    return render(request, 'home_page.html', context)
+
+
+def about_page(request):
+    context = {
+        "title": "About Page",
+        "content": "Welcome to the about page"
+    }
+    return render(request, 'home_page.html', context)
+
+
+def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
+    context = {
+        "title": "Contact Page",
+        "content": "Welcome to the contact page",
+        "form": contact_form
+    }
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+    # if request.method == 'POST':
+    #     print(request.POST.get('fullname'))
+    #     print(request.POST.get('email'))
+    #     print(request.POST.get('content'))
+    return render(request, 'contact/view.html', context)
