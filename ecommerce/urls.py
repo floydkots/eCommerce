c726bb76788ce758536741f5f745e257cbdae66d
Floyd Kots <floydkotohoyoh@gmail.com>
2018-01-23 11:05:18 +0300

    Fully integrated bootstrap

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -13,14 +13,31 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.conf.urls import url, include
 from django.contrib import admin
 
-from .views import home_page, about_page, contact_page
+from .views import home_page, about_page, contact_page, login_page, register_page
 
 urlpatterns = [
-    url(r'^$', home_page),
-    url(r'^about/$', about_page),
-    url(r'^contact/$', contact_page),
+    url(r'^$', home_page, name='home'),
+    url(r'^about/$', about_page, name='about'),
+    url(r'^contact/$', contact_page, name='contact'),
+    url(r'^register/$', register_page, name='register'),
+    url(r'^login/$', login_page, name='login'),
+    url(r'^products/', include('products.urls', namespace='products')),
     url(r'^admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.STATIC_URL,
+        document_root=settings.STATIC_ROOT
+    )
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,13 +1,62 @@
-from django.shortcuts import render
-from .forms import ContactForm
+from django.contrib.auth import authenticate, login, get_user_model
+from django.shortcuts import render, redirect
+from .forms import ContactForm, LoginForm, RegisterForm
 
 
 def home_page(request):
     context = {
-        "title": "Home Page",
-        "content": "Welcome to the homepage"
+        "title": "Home",
+        "content": "Welcome to the homepage",
     }
+    if request.user.is_authenticated():
+        context['premium_content'] = 'Premium Content'
     return render(request, 'home_page.html', context)
+
+
+def login_page(request):
+    form = LoginForm(request.POST or None)
+    context = {
+        'form': form
+    }
+    print('User logged in')
+    # print(request.user.is_authenticated())
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(request,
+                            username=username,
+                            password=password)
+        print(user)
+        # print(request.user.is_authenticated())
+        if user is not None:
+            print(request.user.is_authenticated())
+            login(request, user)
+            # Redirect to a success page.
+            context['form'] = LoginForm()
+            return redirect('/login')
+        else:
+            # Return an 'invalid login' error message.
+            print('Error')
+
+    return render(request, 'auth/login.html', context)
+
+
+def register_page(request):
+    form = RegisterForm(request.POST or None)
+    context = {
+        'form': form
+    }
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get('username')
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password')
+        context['form'] = RegisterForm()
+        user = get_user_model()
+        new_user = user.objects.create_user(username, email, password)
+        print(new_user)
+    return render(request, 'auth/register.html', context)
 
 
 def about_page(request):
@@ -21,7 +70,7 @@ def about_page(request):
 def contact_page(request):
     contact_form = ContactForm(request.POST or None)
     context = {
-        "title": "Contact Page",
+        "title": "Contact",
         "content": "Welcome to the contact page",
         "form": contact_form
     }
