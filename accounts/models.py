78c4d9e93403cd0af802e4d149be878c11345815
Floyd Kots <floydkotohoyoh@gmail.com>
2018-01-31 16:37:58 +0300

    Created the custom User Model Manager

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,41 @@
 from django.db import models
-from django.contrib.auth.models import  (
-    AbstractBaseUser
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager
 )
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, is_active=True, is_staff=False, is_admin=False):
+        if not email:
+            raise ValueError('Users must have an email address')
+        if not password:
+            raise ValueError('Users must have a password')
+        user = self.model(
+            email=self.normalize_email(email)
+        )
+        user.set_password(password)
+        user.staff = is_staff
+        user.admin = is_admin
+        user.active = is_active
+        user.save(using=self._db)
+        return user
+
+    def create_staffuser(self, email, password=None):
+        user = self.create_user(
+            email,
+            password=password,
+            is_staff=True
+        )
+        return user
+
+    def create_superuser(self, email, password=None):
+        user = self.create_user(
+            email,
+            password=password,
+            is_staff=True,
+            is_admin=True
+        )
+        return user
 
 
 class User(AbstractBaseUser):
@@ -16,6 +50,8 @@ class User(AbstractBaseUser):
     # USERNAME_FIELD and password are required by default
 
     REQUIRED_FIELDS = []
+
+    objects = UserManager
 
     def __str__(self):
         return self.email
