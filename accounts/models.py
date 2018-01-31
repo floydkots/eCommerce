b9106b5c1ae1f3cbad93949c1c1d681afb81de23
Floyd Kots <floydkotohoyoh@gmail.com>
2018-01-31 16:27:41 +0300

    Created the custom User Model

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import  (
+    AbstractBaseUser
+)
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=255, unique=True)
+    # full_name = models.CharField(max_length=255, blank=True, null=True)
+    active = models.BooleanField(default=True)
+    staff = models.BooleanField(default=False)
+    admin = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    # USERNAME_FIELD and password are required by default
+
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        return self.email
+
+    def get_short_name(self):
+        return self.email
+
+    @property
+    def is_staff(self):
+        return self.staff
+
+    @property
+    def is_admin(self):
+        return self.admin
+
+    @property
+    def is_active(self):
+        return self.is_active
 
 
 class GuestEmail(models.Model):
