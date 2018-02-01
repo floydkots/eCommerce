8a0067af69184df3e25a95edf979ed04190dfec1
Floyd Kots <floydkotohoyoh@gmail.com>
2018-02-01 14:38:48 +0300

    Added the payment-method view and stripe js

--- billing/views.py
+++ billing/views.py
@@ -1,3 +1,16 @@
+import stripe
+
+from django.conf import settings
+
 from django.shortcuts import render
 
-# Create your views here.
+stripe.api_key = getattr(settings, 'STRIPE_API_KEY')
+STRIPE_PUBLISHABLE_KEY = 'pk_test_Y8MMHpR0P7ynnNCmkP6gdFuw'
+
+
+def payment_method_view(request):
+    if request.method == 'POST':
+        print(request.POST)
+    return render(request, 'billing/payment-method.html', {
+        'publish_key': STRIPE_PUBLISHABLE_KEY
+    })
